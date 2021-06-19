9387b67dc903dfcdcdb2c5af7d9eae4fed29655b
swati2904 <swati.lpu17@gmail.com>
2021-06-19 22:59:59 +0530

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-unused-vars */
-
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, { likes }) => sum + likes
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
