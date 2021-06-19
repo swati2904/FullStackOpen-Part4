964ae52d76bbb38fa1b86755eb8dad0eaa7b99b2
swati2904 <swati.lpu17@gmail.com>
2021-06-19 10:25:16 +0530

    Exercises 4.1.-4.2.

---
+++ app.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+const cors = require('cors');
+const morgan = require('morgan');
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+
+const express = require("express");
+const app = express();
+
+const blogRouter = require('./controllers/blogs');
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("connected to mongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+  app.use(cors());
+  app.use(express.static('build'));
+  morgan.token('body', (req) => JSON.stringify(req.body));
+  app.use(
+    morgan(
+      ':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'
+    )
+  );
+  app.use(express.json());
+
+  app.use("/api/blogs", blogRouter);
+  // handler of requests with unknown endpoint
+app.use(middleware.unknownEndpoint);
+// handler of requests with result to errors
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ controllers/blogs.js
@@ -0,0 +1,22 @@
+const Blog = require("../models/blog");
+const blogRouter = require("express").Router();
+
+blogRouter.get("/",(request, response, next) =>{
+    Blog.find({}).then((blogs) => {
+        response.send(blogs)
+    })
+})
+
+blogRouter.post("/", (request, response, next) => {
+    const blog = new Blog(request.body)
+    blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch((error) => next(error));
+})
+
+
+
+module.exports = blogRouter;
