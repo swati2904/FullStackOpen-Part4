10ab9615c4a9e7ad1e3d62ff99e975f2bb616ce3
swati2904 <swati.lpu17@gmail.com>
2021-06-19 23:41:17 +0530

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const favLikes = blogs.map((blog) => blog.likes)
+  const maxNumberOfLikes = Math.max(...favLikes)
+  const mostBlogLikes = blogs.find((blog) => blog.likes === maxNumberOfLikes)
+  if (mostBlogLikes) {
+    const { title, author, likes } = mostBlogLikes
+    return { title, author, likes }
+  } 
+  return
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
